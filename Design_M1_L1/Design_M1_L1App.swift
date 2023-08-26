555952420fcf255170c3a86a26e6fd9545d21704
Tom Montgomery <montgomery3681@gmail.com>
2023-08-25 22:58:30 -0600

    Initial Commit

---
+++ Design_M1_L1/Design_M1_L1App.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M1_L1App.swift
+//  Design_M1_L1
+//
+//  Created by tom montgomery on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M1_L1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
